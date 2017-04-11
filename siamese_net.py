55316a80c054765182e0d6de0581a173923807d3
www0wwwjs1 <zhangsuofei_1982@hotmail.com>
2017-04-11 11:54:39 +0800

    slice+concat, xcorr, adjust

--- siamese_net.py
+++ siamese_net.py
@@ -25,14 +25,31 @@ def buildNetwork(exemplar, instance, isTraining):
         scope.reuse_variables()
 
         # 直接用tf的conv2d来实现xcorr，从原理上讲，conv2d就是互相关，就是要把滤波器tensor转置一下，变成一个weights的tensor，但是不知道会不会有问题，先写出来再说了
+        # 采用了一样的分片机制来处理不同尺度（batch）的图片，这里只实现了3个尺度，但如果有多张图片是怎么弄得还不知道？？
+    with tf.variable_scope('scorr'):
         print "Building xcorr..."
         aFeat = tf.transpose(aFeat, perm=[1, 2, 3, 0])
-        bFaet = tf.nn.conv2d(score, aFeat, strides=[1, 1, 1, 1])
+        shapeAFeat = aFeat.get_shape()
+        aFeat0 = tf.slice(aFeat, [0, 0, 0, 0], [shapeAFeat[0], shapeAFeat[1], shapeAFeat[2], 1])
+        aFeat1 = tf.slice(aFeat, [0, 0, 0, 1], [shapeAFeat[0], shapeAFeat[1], shapeAFeat[2], 1])
+        aFeat2 = tf.slice(aFeat, [0, 0, 0, 2], [shapeAFeat[0], shapeAFeat[1], shapeAFeat[2], 1])
 
+        shapeScore = score.get_shape()
+        score0 = tf.slice(score, [0, 0, 0, 0], [1, shapeScore[1], shapeScore[2], shapeScore[3]])
+        score1 = tf.slice(score, [1, 0, 0, 0], [1, shapeScore[1], shapeScore[2], shapeScore[3]])
+        score2 = tf.slice(score, [2, 0, 0, 0], [1, shapeScore[1], shapeScore[2], shapeScore[3]])
+
+        score0 = tf.nn.conv2d(score0, aFeat0, strides=[1, 1, 1, 1])
+        score1 = tf.nn.conv2d(score1, aFeat0, strides=[1, 1, 1, 1])
+        score2 = tf.nn.conv2d(score2, aFeat0, strides=[1, 1, 1, 1])
+
+        score = tf.concat([score0, score1, score2], 0)
+
+    with tf.variable_scope('adjust'):
         print "Building adjust..."
         weights = tf.get_variable('weights', [1, 1, 1, 1], initializer=tf.constant_initializer(value=0.001, dtype=tf.float32))
         biases = tf.get_variable('biases', [1, ], initializer=tf.constant_initializer(value=0, dtype=tf.float32))
-        score = tf.nn.conv2d(bFaet, weights, strides=[1, 1, 1, 1])
+        score = tf.nn.conv2d(score, weights, strides=[1, 1, 1, 1])
         score = tf.add(score, biases)
 
     return score
@@ -42,42 +59,62 @@ def buildBranch(inputs, isTraining):
 
     with tf.variable_scope('scala1'):
         print "Building conv1, bn1, relu1, pooling1..."
-        outputs = conv(inputs, 3, 96, 11, 2)
+        outputs = conv1(inputs, 3, 96, 11, 2)
         outputs = batchNormalization(outputs, isTraining)
         outputs = tf.nn.relu(outputs)
         outputs = maxPool(outputs, 3, 2)
 
     with tf.variable_scope('scala2'):
         print "Building conv2, bn2, relu2, pooling2..."
-        outputs = conv(outputs, 48, 5, 256, 1)
+        outputs = conv2(outputs, 48, 256, 5, 1)
         outputs = batchNormalization(outputs, isTraining)
         outputs = tf.nn.relu(outputs)
         outputs = maxPool(outputs, 3, 2)
 
     with tf.variable_scope('scala3'):
         print "Building conv3, bn3, relu3..."
-        outputs = conv(outputs, 256, 3, 384, 1)
+        outputs = conv1(outputs, 256, 384, 3, 1)
         outputs = batchNormalization(outputs, isTraining)
         outputs = tf.nn.relu(outputs)
 
     with tf.variable_scope('scala4'):
         print "Building conv4, bn4, relu4..."
-        outputs = conv(outputs, 192, 3, 384, 1)
+        outputs = conv2(outputs, 192, 384, 3, 1)
         outputs = batchNormalization(outputs, isTraining)
         outputs = tf.nn.relu(outputs)
 
     with tf.variable_scope('scala5'):
         print "Building conv5..."
-        outputs = conv(outputs, 192, 3, 256, 1)
+        outputs = conv2(outputs, 192, 256, 3, 1)
 
     return outputs
 
-def conv(inputs, channels, filters, size, stride):
+def conv1(inputs, channels, filters, size, stride):
     # xavier初始化和截断正态分布初始化，matlab版本用的是一种改进的xavier初始化，如果训练不行，这里可能要调
     weights = tf.get_variable('weights', [size, size, channels, filters], initializer=tf.contrib.layers.xavier_initializer_conv2d(), dtype=tf.float32)
     biases = tf.get_variable('biases', [filters,], initializer=tf.constant_initializer(value=0.1, dtype=tf.float32))
 
     conv = tf.nn.conv2d(inputs, weights, strides=[1, stride, stride, 1])
+    conv = tf.add(conv, biases)
+    print 'Layer Type = Conv, Size = %d * %d, Stride = %d, Filters = %d, Input channels = %d' % (size, size, stride, filters, channels)
+
+    return conv
+
+# 模仿原始alexnet的双gpu结构，卷积层要分片，才能和siamese-fc原始工程的模型对接，实际上无必要
+def conv2(inputs, channels, filters, size, stride):
+    inputShape = inputs.get_shape()
+
+    inputs0 = tf.slice(inputs, [0, 0, 0, 0], [inputShape[0], inputShape[1], inputShape[2], channels])
+    inputs1 = tf.slice(inputs, [0, 0, 0, channels], [inputShape[0], inputShape[1], inputShape[2], channels])
+
+    weights0 = tf.get_variable('weights0', [size, size, channels, filters/2], initializer=tf.contrib.layers.xavier_initializer_conv2d(), dtype=tf.float32)
+    weights1 = tf.get_variable('weights1', [size, size, channels, filters/2], initializer=tf.contrib.layers.xavier_initializer_conv2d(), dtype=tf.float32)
+
+    conv0 = tf.nn.conv2d(inputs0, weights0, strides=[1, stride, stride, 1])
+    conv1 = tf.nn.conv2d(inputs1, weights1, strides=[1, stride, stride, 1])
+    conv = tf.concat([conv0, conv1], 3)
+
+    biases = tf.get_variable('biases', [filters, ], initializer=tf.constant_initializer(value=0.1, dtype=tf.float32))
     conv = tf.add(conv, biases)
     print 'Layer Type = Conv, Size = %d * %d, Stride = %d, Filters = %d, Input channels = %d' % (size, size, stride, filters, channels)
 
---
+++ train.py
@@ -0,0 +1,10 @@
+# Suofei ZHANG, 2017.
+
+import tensorflow as tf
+
+def main(_):
+
+
+if __name__ == '__main__':
+    tf.app.run()
+
